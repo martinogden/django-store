f0f63851efc2bead0129684475678d7b580aa952
Martin Ogden <martin.ogden@gmail.com>
2011-03-30 23:29:20 +0100

    Add basic development settings

---
+++ settings/development.py
@@ -0,0 +1,16 @@
+from path import path
+from settings.common import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': SITE_ROOT / 'db/development.sqlite',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
