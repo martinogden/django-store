28df4efe6af9f6f94b8cc0026a1b44e6afe0e57b
Martin Ogden <martin.ogden@gmail.com>
2011-04-06 09:47:25 +0100

    Catch expired session / deleted basket, and create another one with a friendly-ish error message. This should rarely happen

--- apps/basket/context_processors.py
+++ apps/basket/context_processors.py
@@ -1,7 +1,17 @@
+from django.contrib import messages
+from django.utils.translation import ugettext as _
+
 from basket.models import Basket
 
+
 def basket(request):
-    basket_id = request.session.get('basket_id')
+    try:
+        basket_id = request.session.get('basket_id')
+        basket = Basket.objects.get(pk=basket_id)
+    except Basket.DoesNotExist: 
+        basket = Basket.objects.create()
+        request.session['basket_id'] = basket.pk
+        messages.error(request, _('Oops, your session has expired, and basket is now empty'))
     return {
-        'basket': Basket.objects.get(pk=basket_id)
+        'basket': basket
     }
