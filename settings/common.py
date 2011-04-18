70970afe691ae354bf93f9614754cd99dd283d70
Martin Ogden <martin.ogden@gmail.com>
2011-04-19 00:27:18 +0100

    Add file logging to settings

--- settings/common.py
+++ settings/common.py
@@ -151,13 +151,22 @@ LOGGING = {
         'mail_admins': {
             'level': 'ERROR',
             'class': 'django.utils.log.AdminEmailHandler'
-        }
+        },
+        'file':{
+            'level':'INFO',
+            'class':'logging.FileHandler',
+            'filename': SITE_ROOT / 'logs/app_log'
+        },
     },
     'loggers': {
         'django.request': {
             'handlers': ['mail_admins'],
             'level': 'ERROR',
             'propagate': True,
+        },
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
         },
     }
 }
